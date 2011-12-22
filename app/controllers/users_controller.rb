5e139705c810b57f9559f519f198b432bedc01a9
Alexey Kalinin <camradal@yahoo.com>
2011-12-22 13:31:21 -0800

    user signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def new
     @title = "Sign up"
+    @user = User.new
   end
 
   def show
@@ -8,4 +9,14 @@ class UsersController < ApplicationController
     @title = @user.name
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = 'Welcome to sample app'
+      redirect_to @user
+    else
+      @title = 'Sign up'
+      render 'new'
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -41,4 +41,53 @@ describe UsersController do
       response.should have_selector("h1>img", :class => "gravatar")
     end
   end
+
+  describe "POST 'create'" do
+
+    describe "failure" do
+
+      before (:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        request.should render_template('new')
+      end
+    end
+
+    describe "success" do
+
+      before (:each) do
+        @attr = { :name => "New User", :email => "newuser@example.com", :password => "secret", :password_confirmation => "secret" }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to user page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should flash success" do
+        post :create, :user => @attr
+        flash[:success].should =~ /Welcome to sample app/i
+      end
+    end
+  end
 end
